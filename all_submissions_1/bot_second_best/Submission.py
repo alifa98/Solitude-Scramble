c37f09f6a22fbdc3c7a288ea34c7eb658f771c73
Ali Faraji <ali@faraji.info>
2025-11-14 18:20:28 -0500

    Fix movement logic in get_action function for accurate direction handling based on player position for bots

--- all_submissions_1/bot_second_best/Submission.py
+++ all_submissions_1/bot_second_best/Submission.py
@@ -9,17 +9,17 @@ def get_action(state, match_history, global_history):
     }
     
     if my_pos == "NORTH":
-        my_options["LEFT"] = scores["NW"]
-        my_options["RIGHT"] = scores["NE"]
-    elif my_pos == "EAST":
         my_options["LEFT"] = scores["NE"]
-        my_options["RIGHT"] = scores["SE"]
-    elif my_pos == "SOUTH":
-        my_options["LEFT"] = scores["SE"]
-        my_options["RIGHT"] = scores["SW"]
-    elif my_pos == "WEST":
-        my_options["LEFT"] = scores["SW"]
         my_options["RIGHT"] = scores["NW"]
+    elif my_pos == "EAST":
+        my_options["LEFT"] = scores["SE"]
+        my_options["RIGHT"] = scores["NE"]
+    elif my_pos == "SOUTH":
+        my_options["LEFT"] = scores["SW"]
+        my_options["RIGHT"] = scores["SE"]
+    elif my_pos == "WEST":
+        my_options["LEFT"] = scores["NW"]
+        my_options["RIGHT"] = scores["SW"]
 
     # 2. Find the best move among my options
     # This finds the key (move) with the maximum value (score)
